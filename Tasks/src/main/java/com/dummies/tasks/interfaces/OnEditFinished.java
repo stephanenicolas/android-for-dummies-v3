4bd5d7a3d7a09eb5653db8c9ca1b0a36ac5cf59f
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-09-08 08:21:32 -0400

    Update OnEditFinished.java

--- Tasks/src/main/java/com/dummies/tasks/interfaces/OnEditFinished.java
+++ Tasks/src/main/java/com/dummies/tasks/interfaces/OnEditFinished.java
@@ -1,5 +1,11 @@
 package com.dummies.tasks.interfaces;
 
+//Just a think about : 
+//this is the conventional way of communicating with activity
+//but it forces activities to implement the callback
+//a normal listener interface would require a bit more code to set a listener
+//but would give more flexibility and make the fragments usable outside 
+//of activities (e.g. as nested fragments)
 public interface OnEditFinished {
     /**
      * Called when the user finishes editing a task.
